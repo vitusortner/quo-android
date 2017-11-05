2afd595059c97a34d41a90c53d1418535dfaec1b
vitusortner <vitusortner.dev@gmail.com>
2017-11-05 16:34:27 +0100

    Cleaned onViewCreated method of fragment, added docu

--- app/src/main/java/com/android/quo/view/home/HomeFragment.kt
+++ app/src/main/java/com/android/quo/view/home/HomeFragment.kt
@@ -21,22 +21,36 @@ import kotlinx.android.synthetic.main.fragment_home.placePreviewRecyclerView
 
 class HomeFragment : Fragment() {
 
+    private lateinit var placePreviewListViewModel: PlacePreviewListViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         inflater.inflate(R.layout.fragment_home, container, false)
 
-    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val placePreviewListViewModel = ViewModelProviders.of(this)
-            .get(PlacePreviewListViewModel().javaClass)
+        placePreviewListViewModel = ViewModelProviders.of(this).get(PlacePreviewListViewModel().javaClass)
 
+        observePlacePreviewList()
+        setupSwipeRefresh()
+    }
+
+    /**
+     * Observe place preview list and set adapter for place preview recycler view
+     */
+    private fun observePlacePreviewList() {
         placePreviewListViewModel.getPlacePreviewList().observe(this, Observer { list ->
             list?.let {
                 placePreviewRecyclerView.adapter = PlacePreviewAdapter(this.context, list)
                 placePreviewRecyclerView.layoutManager = LinearLayoutManager(this.context)
             }
         })
+    }
 
+    /**
+     * Update place preview list and stop refreshing animation
+     */
+    private fun setupSwipeRefresh() {
         homeSwipeRefreshLayout.setOnRefreshListener {
             placePreviewListViewModel.updatePlacePreviewList()
             homeSwipeRefreshLayout.isRefreshing = false
