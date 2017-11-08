d1e8e5663c5686193e67f8afb604c682d93e7586
vitusortner <vitusortner.dev@gmail.com>
2017-11-08 19:52:10 +0100

    Fixed style to match style guide

--- app/src/main/java/com/android/quo/view/home/PlacePreviewAdapter.kt
+++ app/src/main/java/com/android/quo/view/home/PlacePreviewAdapter.kt
@@ -18,33 +18,38 @@ import kotlinx.android.synthetic.main.place_preview_cardview.placePreviewTitleTe
 /**
  * Created by vitusortner on 27.10.17.
  */
-class PlacePreviewAdapter(private val list: List<PlacePreview>) : RecyclerView.Adapter<PlacePreviewAdapter.PlacePreviewViewHolder>() {
+class PlacePreviewAdapter(private val list: List<PlacePreview>) :
+        RecyclerView.Adapter<PlacePreviewAdapter.PlacePreviewViewHolder>() {
 
-    override fun onBindViewHolder(holder: PlacePreviewViewHolder, position: Int) = holder.bindItem(list, position)
+    override fun onBindViewHolder(holder: PlacePreviewViewHolder, position: Int) =
+            holder.bindItem(list, position)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlacePreviewViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.place_preview_cardview, parent, false)
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.place_preview_cardview,
+                parent, false)
         return PlacePreviewViewHolder(itemView)
     }
 
     override fun getItemCount(): Int = list.size
 
-    class PlacePreviewViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+    class PlacePreviewViewHolder(override val containerView: View) :
+            RecyclerView.ViewHolder(containerView), LayoutContainer {
 
         fun bindItem(list: List<PlacePreview>, position: Int) {
             val imageUrl = list[position].imageUrl
 
             Glide.with(containerView.context)
-                .load(imageUrl)
-                // Set card view background color with Palette
-                .listener(GlidePalette.with(imageUrl)
-                    .intoCallBack { palette ->
-                        palette?.let {
-                            placePreviewCardView.setCardBackgroundColor(palette.getMutedColor(BitmapPalette.Profile.MUTED))
-                        }
-                    }
-                )
-                .into(placePreviewImageView)
+                    .load(imageUrl)
+                    // Set card view background color with Palette
+                    .listener(GlidePalette.with(imageUrl)
+                            .intoCallBack { palette ->
+                                palette?.let {
+                                    placePreviewCardView.setCardBackgroundColor(
+                                            palette.getMutedColor(BitmapPalette.Profile.MUTED))
+                                }
+                            }
+                    )
+                    .into(placePreviewImageView)
 
             placePreviewTitleTextView.text = list[position].title
             placePreviewDescriptionTextView.text = list[position].description
