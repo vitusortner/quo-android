b216081dc9123eb243f4c49dafb01da88bd81ad4
vitusortner <vitusortner.dev@gmail.com>
2017-11-10 10:39:13 +0100

    Removed empty class

--- app/src/main/java/com/android/quo/view/gallery/Gallery.kt
+++
@@ -1,7 +0,0 @@
-package com.android.quo.view.gallery
-
-/**
- * Created by vitusortner on 08.11.17.
- */
-// this has to be set in onCreateView of Gallery fragment to allow screen roation
-// activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_FULL_USER
