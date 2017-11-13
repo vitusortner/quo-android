d3369b0defb3e52d52398b4c8f05fa0502fa2f2f
Stefan Jung <stefan@theappbusiness.com>
2017-11-13 12:40:22 +0100

    changed to focus lost for login email and password

--- app/src/main/java/com/android/quo/view/login/LoginActivity.kt
+++ app/src/main/java/com/android/quo/view/login/LoginActivity.kt
@@ -1,20 +1,26 @@
 package com.android.quo.view.login
 
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.support.v4.view.ViewCompat
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.text.Editable
-import android.text.TextWatcher
-import android.widget.EditText
 import com.android.quo.R
+import com.android.quo.R.style.EditTextTheme
 import com.android.quo.viewmodel.LoginViewModel
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import kotlinx.android.synthetic.main.activity_login.forgotPasswordClickableTextView
 import kotlinx.android.synthetic.main.activity_login.loginEmailEditText
 import kotlinx.android.synthetic.main.activity_login.loginErrorEmailMessage
 import kotlinx.android.synthetic.main.activity_login.loginErrorPasswordMessage
 import kotlinx.android.synthetic.main.activity_login.loginPasswordEditText
-import kotlinx.android.synthetic.main.activity_login.registerButton
+import kotlinx.android.synthetic.main.activity_login.signUpButton
 import kotlinx.android.synthetic.main.layout_forgot_password.view.errorEmail
 import kotlinx.android.synthetic.main.layout_forgot_password.view.textInputEmail
 
@@ -24,13 +30,16 @@ import kotlinx.android.synthetic.main.layout_forgot_password.view.textInputEmail
  */
 class LoginActivity : AppCompatActivity() {
     private lateinit var loginViewModel: LoginViewModel
+    private lateinit var callbackManager: CallbackManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        callbackManager = CallbackManager.Factory.create()
+
         loginViewModel = ViewModelProviders.of(this).get(LoginViewModel().javaClass)
 
-        registerButton.setOnClickListener {
+        signUpButton.setOnClickListener {
             if (loginViewModel.verifyEmail(loginEmailEditText.text.toString()) && loginViewModel
                     .verifyPassword(loginPasswordEditText.text.toString())) {
                 loginViewModel.handleRegister(loginEmailEditText.text.toString(),
@@ -44,22 +53,56 @@ class LoginActivity : AppCompatActivity() {
             openDialogForgotPassword()
         }
 
-        loginEmailEditText.afterTextChanged {
-            if (loginViewModel.verifyEmail(loginEmailEditText.text.toString())) {
-                loginErrorEmailMessage.text = ""
-            } else {
-                loginErrorEmailMessage.text = resources.getString(R.string.error_email)
+
+        loginEmailEditText.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                if (loginViewModel.verifyEmail(loginEmailEditText.text.toString())) {
+                    loginErrorEmailMessage.text = ""
+                    ViewCompat.setBackgroundTintList(loginEmailEditText, ColorStateList
+                            .valueOf(EditTextTheme))
+                } else {
+                    loginErrorEmailMessage.text = resources.getString(R.string.error_email)
+                    ViewCompat.setBackgroundTintList(loginEmailEditText, ColorStateList
+                            .valueOf(getColor(R.color.colorErrorRed)))
+                }
             }
         }
 
-        loginPasswordEditText.afterTextChanged {
-            if (loginViewModel.verifyPassword(loginPasswordEditText.text.toString())) {
-                loginErrorPasswordMessage.text = ""
-            } else {
-                loginErrorPasswordMessage.text = resources.getString(R.string.error_password)
+        loginPasswordEditText.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                if (loginViewModel.verifyPassword(loginPasswordEditText.text.toString())) {
+                    loginErrorPasswordMessage.text = ""
+                    ViewCompat.setBackgroundTintList(loginPasswordEditText, ColorStateList
+                            .valueOf(EditTextTheme))
+                } else {
+                    loginErrorPasswordMessage.text = resources.getString(R.string.error_password)
+                    ViewCompat.setBackgroundTintList(loginPasswordEditText, ColorStateList
+                            .valueOf(getColor(R.color.colorErrorRed)))
+                }
             }
         }
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                object : FacebookCallback<LoginResult> {
+                    override fun onSuccess(loginResult: LoginResult) {
+                        // App code
+                    }
+
+                    override fun onCancel() {
+                        // App code
+                    }
+
+                    override fun onError(exception: FacebookException) {
+                        // App code
+                    }
+                })
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
 
     private fun openAlertCheckInput() {
         val dialog = AlertDialog.Builder(this).create()
@@ -75,11 +118,13 @@ class LoginActivity : AppCompatActivity() {
         dialog.setTitle(resources.getString(R.string.forgot_password))
         dialog.setView(dialogView)
 
-        dialogView.textInputEmail.afterTextChanged {
-            if (loginViewModel.verifyEmail(loginEmailEditText.text.toString())) {
-                loginErrorEmailMessage.text = ""
-            } else {
-                loginErrorEmailMessage.text = resources.getString(R.string.error_email)
+        dialogView.textInputEmail.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                if (loginViewModel.verifyEmail(loginEmailEditText.text.toString())) {
+                    loginErrorEmailMessage.text = ""
+                } else {
+                    loginErrorEmailMessage.text = resources.getString(R.string.error_email)
+                }
             }
         }
 
@@ -89,6 +134,7 @@ class LoginActivity : AppCompatActivity() {
                 if (loginViewModel.verifyEmail(dialogView.textInputEmail.text.toString())) {
                     dialogView.errorEmail.text = ""
                     dialog.dismiss()
+                    openDialogPasswordResetFinished()
                 } else {
                     dialogView.errorEmail.text = resources.getString(R.string.error_email)
                 }
@@ -97,7 +143,6 @@ class LoginActivity : AppCompatActivity() {
 
         dialog.setButton(AlertDialog.BUTTON_POSITIVE, resources.getString(R.string.next), { _, _ ->
             if (loginViewModel.sendEmailToUser(dialogView.textInputEmail.text.toString())) {
-                openDialogPasswordResetFinished()
             }
         })
         dialog.show()
@@ -108,17 +153,5 @@ class LoginActivity : AppCompatActivity() {
         dialog.setTitle(resources.getString(R.string.send_reset_email_title))
         dialog.setMessage(resources.getString(R.string.send_reset_email_message))
         dialog.show()
-    }
-
-    private fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
-        this.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-            override fun afterTextChanged(editable: Editable?) {
-                afterTextChanged.invoke(editable.toString())
-            }
-        })
     }
 }
