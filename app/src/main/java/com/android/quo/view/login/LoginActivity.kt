f2f6410f16a4011c96adef76e451dd2b48cf63ae
Stefan Jung <stefan@theappbusiness.com>
2017-11-14 21:32:47 +0100

    added layout properties in values files

--- app/src/main/java/com/android/quo/view/login/LoginActivity.kt
+++ app/src/main/java/com/android/quo/view/login/LoginActivity.kt
@@ -15,14 +15,15 @@ import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
+import kotlinx.android.synthetic.main.activity_login.EmailEditText
+import kotlinx.android.synthetic.main.activity_login.ErrorEmailMessage
+import kotlinx.android.synthetic.main.activity_login.ErrorPasswordMessage
+import kotlinx.android.synthetic.main.activity_login.PasswordEditText
 import kotlinx.android.synthetic.main.activity_login.forgotPasswordClickableTextView
-import kotlinx.android.synthetic.main.activity_login.loginEmailEditText
-import kotlinx.android.synthetic.main.activity_login.loginErrorEmailMessage
-import kotlinx.android.synthetic.main.activity_login.loginErrorPasswordMessage
-import kotlinx.android.synthetic.main.activity_login.loginPasswordEditText
 import kotlinx.android.synthetic.main.activity_login.signUpButton
 import kotlinx.android.synthetic.main.layout_forgot_password.view.errorEmail
 import kotlinx.android.synthetic.main.layout_forgot_password.view.textInputEmail
+import kotlinx.android.synthetic.main.layout_sign_up.view.agreement_checkbox
 
 
 /**
@@ -40,10 +41,10 @@ class LoginActivity : AppCompatActivity() {
         loginViewModel = ViewModelProviders.of(this).get(LoginViewModel().javaClass)
 
         signUpButton.setOnClickListener {
-            if (loginViewModel.verifyEmail(loginEmailEditText.text.toString()) && loginViewModel
-                    .verifyPassword(loginPasswordEditText.text.toString())) {
-                loginViewModel.handleRegister(loginEmailEditText.text.toString(),
-                        loginPasswordEditText.text.toString())
+            if (loginViewModel.verifyEmail(EmailEditText.text.toString()) && loginViewModel
+                    .verifyPassword(PasswordEditText.text.toString())) {
+                loginViewModel.handleRegister(EmailEditText.text.toString(),
+                        PasswordEditText.text.toString())
             } else {
                 openAlertCheckInput()
             }
@@ -53,35 +54,48 @@ class LoginActivity : AppCompatActivity() {
             openDialogForgotPassword()
         }
 
+        signUpButton.setOnClickListener { openDialogSignUp() }
 
-        loginEmailEditText.setOnFocusChangeListener { _, hasFocus ->
+
+        /**
+         * check if email is validate
+         */
+        EmailEditText.setOnFocusChangeListener { _, hasFocus ->
             if (!hasFocus) {
-                if (loginViewModel.verifyEmail(loginEmailEditText.text.toString())) {
-                    loginErrorEmailMessage.text = ""
-                    ViewCompat.setBackgroundTintList(loginEmailEditText, ColorStateList
+                if (loginViewModel.verifyEmail(EmailEditText.text.toString())
+                        || EmailEditText.text.isEmpty()) {
+                    ErrorEmailMessage.text = ""
+                    ViewCompat.setBackgroundTintList(EmailEditText, ColorStateList
                             .valueOf(EditTextTheme))
                 } else {
-                    loginErrorEmailMessage.text = resources.getString(R.string.error_email)
-                    ViewCompat.setBackgroundTintList(loginEmailEditText, ColorStateList
+                    ErrorEmailMessage.text = resources.getString(R.string.error_email)
+                    ViewCompat.setBackgroundTintList(EmailEditText, ColorStateList
                             .valueOf(getColor(R.color.colorErrorRed)))
                 }
             }
         }
 
-        loginPasswordEditText.setOnFocusChangeListener { _, hasFocus ->
+        /**
+         * check if password is validate
+         */
+        PasswordEditText.setOnFocusChangeListener { _, hasFocus ->
             if (!hasFocus) {
-                if (loginViewModel.verifyPassword(loginPasswordEditText.text.toString())) {
-                    loginErrorPasswordMessage.text = ""
-                    ViewCompat.setBackgroundTintList(loginPasswordEditText, ColorStateList
+                if (loginViewModel.verifyPassword(PasswordEditText.text.toString())
+                        || PasswordEditText.text.isEmpty()) {
+                    ErrorPasswordMessage.text = ""
+                    ViewCompat.setBackgroundTintList(PasswordEditText, ColorStateList
                             .valueOf(EditTextTheme))
                 } else {
-                    loginErrorPasswordMessage.text = resources.getString(R.string.error_password)
-                    ViewCompat.setBackgroundTintList(loginPasswordEditText, ColorStateList
+                    ErrorPasswordMessage.text = resources.getString(R.string.error_password)
+                    ViewCompat.setBackgroundTintList(PasswordEditText, ColorStateList
                             .valueOf(getColor(R.color.colorErrorRed)))
                 }
             }
         }
 
+        /**
+         * Handle Facebook result
+         */
         LoginManager.getInstance().registerCallback(callbackManager,
                 object : FacebookCallback<LoginResult> {
                     override fun onSuccess(loginResult: LoginResult) {
@@ -105,7 +119,7 @@ class LoginActivity : AppCompatActivity() {
 
 
     private fun openAlertCheckInput() {
-        val dialog = AlertDialog.Builder(this).create()
+        val dialog = AlertDialog.Builder(this, R.style.AlertDialogTheme).create()
         dialog.setTitle(resources.getString(R.string.registration_failed_title))
         dialog.setMessage(resources.getString(R.string.registration_failed_message))
         dialog.setButton(AlertDialog.BUTTON_POSITIVE, resources.getString(R.string.ok), { _, _ -> })
@@ -113,30 +127,24 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun openDialogForgotPassword() {
-        val dialog = AlertDialog.Builder(this).create()
+        val dialog = AlertDialog.Builder(this, R.style.AlertDialogTheme).create()
         val dialogView = layoutInflater.inflate(R.layout.layout_forgot_password, null)
         dialog.setTitle(resources.getString(R.string.forgot_password))
         dialog.setView(dialogView)
-
-        dialogView.textInputEmail.setOnFocusChangeListener { _, hasFocus ->
-            if (!hasFocus) {
-                if (loginViewModel.verifyEmail(loginEmailEditText.text.toString())) {
-                    loginErrorEmailMessage.text = ""
-                } else {
-                    loginErrorEmailMessage.text = resources.getString(R.string.error_email)
-                }
-            }
-        }
 
         dialog.setOnShowListener({ dialog ->
             val button = (dialog as AlertDialog).getButton(AlertDialog.BUTTON_POSITIVE)
             button.setOnClickListener {
                 if (loginViewModel.verifyEmail(dialogView.textInputEmail.text.toString())) {
                     dialogView.errorEmail.text = ""
+                    ViewCompat.setBackgroundTintList(dialogView.textInputEmail, ColorStateList
+                            .valueOf(EditTextTheme))
                     dialog.dismiss()
                     openDialogPasswordResetFinished()
                 } else {
                     dialogView.errorEmail.text = resources.getString(R.string.error_email)
+                    ViewCompat.setBackgroundTintList(dialogView.textInputEmail, ColorStateList
+                            .valueOf(getColor(R.color.colorErrorRed)))
                 }
             }
         })
@@ -149,9 +157,34 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun openDialogPasswordResetFinished() {
-        val dialog = AlertDialog.Builder(this).create()
+        val dialog = AlertDialog.Builder(this, R.style.AlertDialogTheme).create()
         dialog.setTitle(resources.getString(R.string.send_reset_email_title))
         dialog.setMessage(resources.getString(R.string.send_reset_email_message))
+        dialog.show()
+    }
+
+    private fun openDialogSignUp() {
+        val dialog = AlertDialog.Builder(this, R.style.AlertDialogTheme).create()
+        val dialogView = layoutInflater.inflate(R.layout.layout_sign_up, null)
+        dialog.setTitle(resources.getString(R.string.sign_up))
+        dialog.setView(dialogView)
+
+        dialog.setOnShowListener({ dialog ->
+            val button = (dialog as AlertDialog).getButton(AlertDialog.BUTTON_POSITIVE)
+            button.setOnClickListener {
+                if (dialogView.agreement_checkbox.isActivated) {
+                    dialog.dismiss()
+                } else {
+                    dialogView.agreement_checkbox.setTextColor(getColor(R.color.colorErrorRed))
+                }
+            }
+        })
+
+        dialog.setButton(AlertDialog.BUTTON_POSITIVE, resources.getString(R.string.next), { _, _ ->
+
+        })
+
+        dialog.setButton(AlertDialog.BUTTON_NEGATIVE, resources.getString(R.string.cancel), { _, _ -> })
         dialog.show()
     }
 }
