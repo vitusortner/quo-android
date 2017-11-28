b83eeafbf55d4933403b3628583cbbc66260aa4d
Stefan Jung <stefan@theappbusiness.com>
2017-11-28 22:57:02 +0100

    implemented page and style of settings

--- app/src/main/java/com/android/quo/view/myplaces/CreatePageFragment.kt
+++ app/src/main/java/com/android/quo/view/myplaces/CreatePageFragment.kt
@@ -1,19 +1,30 @@
 package com.android.quo.view.myplaces
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import com.android.quo.R
-import kotlinx.android.synthetic.main.fragment_my_places.floatingActionButton
+import com.jakewharton.rxbinding2.view.RxView
+import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.fragment_create_page.floatingActionButton
+import kotlinx.android.synthetic.main.fragment_create_page.mainEditText
+import kotlinx.android.synthetic.main.fragment_create_page.roundEditButton
+import kotlinx.android.synthetic.main.fragment_create_page.roundGalleryButton
+import kotlinx.android.synthetic.main.fragment_create_page.tempLine1TextView
+import kotlinx.android.synthetic.main.fragment_create_page.tempLine2TextView
+
 
 /**
  * Created by Jung on 27.11.17.
  */
 
-class CreatePageFragment: Fragment(){
+class CreatePageFragment : Fragment() {
+    private var compositeDisposable = CompositeDisposable()
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -22,15 +33,35 @@ class CreatePageFragment: Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupFloatingActionButton()
+        compositeDisposable.add(RxView.clicks(floatingActionButton)
+                .subscribe {
+                    roundEditButton.visibility = VISIBLE
+                    roundGalleryButton.visibility = VISIBLE
+                })
+
+        compositeDisposable.add(RxView.clicks(mainEditText)
+                .subscribe {
+                    if (roundEditButton.isEnabled && roundGalleryButton.isEnabled)
+                        roundEditButton.visibility = GONE
+                    roundGalleryButton.visibility = GONE
+                })
+
+        compositeDisposable.add(RxView.clicks(roundEditButton)
+                .subscribe {
+                    tempLine1TextView.visibility = GONE
+                    tempLine2TextView.visibility = GONE
+
+                    mainEditText.isClickable = true
+                    mainEditText.isFocusable = true
+                    mainEditText.isFocusableInTouchMode = true
+                    mainEditText.isCursorVisible = true
+                })
+
+
     }
 
-
-    private fun setupFloatingActionButton() =
-            floatingActionButton.setOnClickListener {
-                Snackbar.make(floatingActionButton, "Floating action button clicked",
-                        Snackbar.LENGTH_LONG)
-                        .setAction("HIDE", { })
-                        .show()
-            }
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
+    }
 }
