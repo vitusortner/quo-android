fa1bdc5c1e0c0d995201946941baf3f08e0eee60
vitusortner <vitusortner.dev@gmail.com>
2018-01-17 20:50:56 +0100

    Adds comment and link to ticket

--- app/src/main/java/com/android/quo/view/place/PlaceFragment.kt
+++ app/src/main/java/com/android/quo/view/place/PlaceFragment.kt
@@ -286,6 +286,8 @@ class PlaceFragment : Fragment() {
                             it?.let {
                                 viewModel.uploadImage(it, placeId)
                                 // TODO hide bottom sheet and refresh gallery
+                                // https://app.clickup.com/751518/751948/t/xazx
+                                // maybe add completionHandler to uploadImage function and update gallery then
                             }
                         }
                     }
